304ae07afb783f703e91f131f7d8120f0701f928
stpafk <stephan.weigert@hotmail.com>
2023-09-30 18:56:36 -0300

    add case in setRequest

--- marketplace/src/utils/setRequest.jsx
+++ marketplace/src/utils/setRequest.jsx
@@ -4,13 +4,23 @@ function setRequest(type) {
     let request = ""
 
     switch(type) {
-        case 'top': request = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=heavy+metal&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=5";
+        case 'heavy': request = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=heavy+metal&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=25";
         break;
-    default:
+        case 'rock': request = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=rock&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=25"
+        break;
+        case 'hardcore': request = 'http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=deathcore&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=25'
+        break;
+        case 'punk': request = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=punk+rock&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=25"
+        break;
+        case 'death': request = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=death+metal&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=25"
+        break;
+        case 'black': request = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=black+metal&api_key=3f26f648032bcdf951aa7395619343a3&format=json&limit=25"
+        break;
+        default:
         console.log('error')
     }
 
-    return request
+    return request;
 
 }
 
