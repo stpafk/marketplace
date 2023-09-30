afb8af184e97b5bbeb26a17e5595a87ba0111559
stpafk <stephan.weigert@hotmail.com>
2023-09-30 20:24:35 -0300

    change header to handle search

--- marketplace/src/components/Header.jsx
+++ marketplace/src/components/Header.jsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
 
+    const [value, setValue] = useState("");
+    const navigate = useNavigate()
+
+    function handleSearch(event) {
+        event.preventDefault();
+        if (value) {
+            navigate("/search", {type: value, replace: true })
+        };
+    };
+    
     return(
         <section className="header">
               <img src="" alt="A" />
@@ -10,10 +21,11 @@ export default function Header() {
                   <p>Your metal albums marketplace.</p>
               </div>
 
-              <div className="header__search">
-                <label htmlFor="search">Search Album</label>
-                <input type="text" />
-              </div>
+              <form className="header__search" onSubmit={handleSearch}>
+                <label htmlFor="search">Search Album/Artist</label>
+                <input type="text" placeholder="Search" onChange={(e) => setValue(e.target.value)}/>
+                <button type="submit">submit</button>
+              </form>
 
               <div className="header__shop">
                 <span className='add'>
@@ -28,6 +40,5 @@ export default function Header() {
                 </span>
               </div>
           </section>
-    )
-
-}
+    );
+};
