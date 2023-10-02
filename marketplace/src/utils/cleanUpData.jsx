cd72ab9f6f21d0afefe6f6512e58c28c85e641a1
stpafk <stephan.weigert@hotmail.com>
2023-10-02 08:30:56 -0300

    add image cleanup

--- marketplace/src/utils/cleanUpData.jsx
+++ marketplace/src/utils/cleanUpData.jsx
@@ -2,8 +2,9 @@
 export default function cleanUpData(data) {
 
     const unWanted = ['remastered', 'remaster', 'deluxe', 'reissue', 'collection'
-    ,'anniversary', 'expanded']
+    ,'anniversary', 'expanded', '(null)']
     let newObj = data.filter(str => !unWanted.some(term => str.name.toLowerCase().includes(term)));
+    newObj = newObj.filter(str => str.image[3]['#text']!== "") 
 
     return newObj;
 } 
